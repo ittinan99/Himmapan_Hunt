b46f16b04d4aa5e2bb031ef9c8b4e5a5bb384bb4
tewlywth314 <61342905+tewlywth314@users.noreply.github.com>
2021-07-18 16:16:58 +0700

    FinallTest

--- Assets/Script/EnemyPandaTest.cs
+++ Assets/Script/EnemyPandaTest.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 using Panda;
 public class EnemyPandaTest : MonoBehaviour
 {
     public float speed = 10.0f;
-    [Task]
+    public float D = 0f;
+    public Animator anim;
+    public GameObject HeadMark;
+    public GameObject LeftBodyMark;
+    public GameObject RightBodyMark;
+    public GameObject TailMark;
+ 
+    
     void FaceTowardPlayer()
     {
         GameObject player = null;
@@ -14,24 +22,60 @@ public class EnemyPandaTest : MonoBehaviour
         if(player == null)
         {
             Debug.Log("No Player !!");
-            Task.current.Fail();
+            
         }
         else
         {
             Debug.Log("Have Player !!");
             this.gameObject.transform.LookAt(player.transform);
+            
+        }
+    }
+    [Task]
+    void Roar()
+    {
+        Debug.Log("Fooouu  Fouuu ffoofofo fappp !!");
+        Task.current.Succeed();
+
+    }
+    [Task]
+    void PlayerInRanged()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if(Vector3.Distance(player.transform.position,this.gameObject.transform.position) <= D)
+        {
+            Debug.Log("Player In Ranged");
             Task.current.Succeed();
+        }
+        else
+        {
+            Task.current.Fail();
+        }
+    }
+    [Task]
+    void PlayerNotInRanged()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (Vector3.Distance(player.transform.position, this.gameObject.transform.position) > D)
+        {
+            Debug.Log("Player Not In Ranged");
+            Task.current.Succeed();
+        }
+        else
+        {
+            Task.current.Fail();
         }
     }
     [Task]
     void MoveToPlayer()
     {
         GameObject player = GameObject.FindGameObjectWithTag("Player");
+        //FaceTowardPlayer();
         Vector3 destination = player.transform.position;
         Vector3 delta = (destination - transform.position);
         Vector3 velocity = speed * delta.normalized;
 
-        transform.position = transform.position + velocity * Time.deltaTime;
+        
 
         Vector3 newDelta = (destination - transform.position);
         float d = newDelta.magnitude;
@@ -39,15 +83,70 @@ public class EnemyPandaTest : MonoBehaviour
         if (Task.isInspected)
             Task.current.debugInfo = string.Format("d={0:0.000}", d);
 
-        if (Vector3.Dot(delta, newDelta) <= 0.0f || d < 10f)
+        if (d<= D)
         {
-            transform.position = transform.position;
             Task.current.Succeed();
-            d = 0.0f;
-            Task.current.debugInfo = "d=0.000";
+            //d = 0.0f;
+            //Task.current.debugInfo = "d=0.000";
+        }
+        else
+        {
+            //transform.position = transform.position + velocity * Time.deltaTime;
         }
 
     }
+    GameObject NearestPoint()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        float HeadDis = Vector3.Distance(player.transform.position, HeadMark.transform.position);
+        float LeftDis = Vector3.Distance(player.transform.position, LeftBodyMark.transform.position);
+        float RightDis = Vector3.Distance(player.transform.position, RightBodyMark.transform.position);
+        float TailDis = Vector3.Distance(player.transform.position, TailMark.transform.position);
+        float MinDis = Mathf.Min(Mathf.Min(HeadDis,RightDis), Mathf.Min(LeftDis,TailDis));
+        if(MinDis == HeadDis)
+        {
+            return HeadMark;
+        }
+        else if(MinDis == LeftDis)
+        {
+            return LeftBodyMark;
+        }
+        else if (MinDis == RightDis)
+        {
+            return RightBodyMark;
+        }
+        else
+        {
+            return TailMark;
+        }
+    }
+    [Task]
+    void ActionNearestPoint()
+    {
+        GameObject NPoint = NearestPoint();
+        if (NPoint.Equals(HeadMark))
+        {
+            anim.SetTrigger("Front");
 
+            
+        }
+        else if (NPoint.Equals(LeftBodyMark))
+        {
+            anim.SetTrigger("Left");
+        
+        }
+        else if (NPoint.Equals(RightBodyMark))
+        {
+            anim.SetTrigger("Right");
+         
+        }
+        else
+        {
+            anim.SetTrigger("Back");
+
+        }
+        Task.current.Succeed();
+    }
+    
     
 }
